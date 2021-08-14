c369be77b24eaba4ea71ab3d47a6191e89ec4b33
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-08-14 11:56:32 -0500

    setting up router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function App() {
   const weather = [
     { id: 1, city: "Houston", current: "Sunny", temp: "79", hum: "15", wndspd: "15 knts"}
   ]
   return (
-    <div className="App">
-      <h1>Weather Dashboard</h1>
+    <Router>
+      <div className="App">
+        <h1>Weather Dashboard</h1>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/close-up">Day-Cast</Link>
+          </li>
+          <li>
+            <Link to="/current">Current Weather</Link>
+          </li>
+        </ul>
+        <hr />
+        <Dashboard weather={weather} />
 
-      <Dashboard weather={weather} />
-    </div>
+        <Switch>
+          {/* <Route exact path="/">
+            <Dashboard />
+          </Route> */}
+          <Route path="/close-up">
+            <CloseUp />
+          </Route>
+          <Route path="/current">
+            <Current />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
@@ -31,4 +62,20 @@ function Dashboard({ weather }) {
       ))}
     </ul>
   )
+}
+
+function CloseUp() {
+  return (
+    <div>
+      <h1>Day-Cast</h1>
+    </div>
+  );
+}
+
+function Current() {
+  return (
+    <div>
+      <h1>Current Weather</h1>
+    </div>
+  );
 }
